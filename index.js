2acc1bbe1ce885f9c6ddd28c480c5b33ff377a19
teshager <teshager8922@gmail.com>
2024-06-19 10:44:35 +0300

    write multiply and subtract functions

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const add =( num1,num2)=>{
 const subtract =( num1,num2)=>{
     return num1-num2;
 }
+const multiply =( num1,num2)=>{
+    return num1*num2;
+}
+const divide =( num1,num2)=>{
+    return num1/num2;
+}
