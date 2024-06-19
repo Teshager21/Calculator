7cae27d174e1455a539568df35c7f3e8952dd8ef
teshager <teshager8922@gmail.com>
2024-06-19 10:53:23 +0300

    add an operate function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+let num1,num2, operator;
 const add =( num1,num2)=>{
        return num1+num2;
 }
@@ -11,3 +11,20 @@ const multiply =( num1,num2)=>{
 const divide =( num1,num2)=>{
     return num1/num2;
 }
+
+const operate=(num1,num2,operator)=>{
+switch(operator){
+    case '+':
+         return add(num1,num2);
+    case '-':
+    return subtract(num1,num2);
+    case '*':
+        return multiply(num1,num2);
+    case '/':
+        return divide(num1,num2);
+    default:
+        return;
+}
+}
+
+console.log(operate(3,1,'/'));
